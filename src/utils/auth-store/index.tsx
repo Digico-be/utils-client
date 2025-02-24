2538108d1ca00fdab231c286672f326f9e511e8f
Maxime Fondu <maximefondu@icloud.com>
2025-02-24 15:46:30 +0100

    remvoe context for store auth

---
+++ src/utils/auth-store/index.tsx
@@ -0,0 +1,24 @@
+interface UserType {
+    firstname: string
+    lastname: string
+    email: string
+}
+
+interface TenantType {
+    id: string
+    name: string
+}
+
+export const authStore = {
+    user: null as UserType | null,
+    tenant: null as TenantType | null,
+
+    setAuth(user: UserType, tenant: TenantType) {
+        this.user = user
+        this.tenant = tenant
+    },
+
+    getAuth() {
+        return { user: this.user, tenant: this.tenant }
+    }
+}
