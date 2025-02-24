91141a1a2d1a8635a5b85ecfd4ee76ab2770b676
Maxime Fondu <maximefondu@icloud.com>
2025-02-24 15:30:41 +0100

    Add complete context auth++

--- src/utils/auth-context/index.tsx
+++ src/utils/auth-context/index.tsx
@@ -21,14 +21,25 @@ const AuthContext = createContext<
     | undefined
 >(undefined)
 
-export const AuthProvider = ({ children, user, tenant }: { children: ReactNode; tenant: TenantType; user: UserType }) => {
-    return <AuthContext.Provider value={{ user, tenant }}>{children}</AuthContext.Provider>
-}
-
-export const useAuth = () => {
+const initAuth = () => {
     const context = useContext(AuthContext)
     if (!context) {
         throw new Error('useAuth doit être utilisé dans AuthProvider')
     }
     return context
+}
+
+if (typeof globalThis !== 'undefined') {
+    globalThis.__DIGICO_USE_AUTH__ = initAuth
+}
+
+export const AuthProvider = ({ children, user, tenant }: { children: ReactNode; tenant: TenantType; user: UserType }) => {
+    return <AuthContext.Provider value={{ user, tenant }}>{children}</AuthContext.Provider>
+}
+
+export const useAuth = () => {
+    if (typeof globalThis !== 'undefined' && globalThis.__DIGICO_USE_AUTH__) {
+        return globalThis.__DIGICO_USE_AUTH__
+    }
+    throw new Error('useAuth must be provided. Assurez-vous que `AuthProvider` est bien chargé dans votre projet.')
 }
