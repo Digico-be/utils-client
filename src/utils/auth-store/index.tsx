decc46925c2925e24e7dfb2d7978f62f24034a9c
Maxime Fondu <maximefondu@icloud.com>
2025-02-24 16:15:15 +0100

    fix useAuth temp cookie

--- src/utils/auth-store/index.tsx
+++ src/utils/auth-store/index.tsx
@@ -1,31 +1,6 @@
-import { create } from 'zustand'
-
-interface UserType {
-    firstname: string
-    lastname: string
-    email: string
+import cookies from 'js-cookie'
+export const useAuth = () => {
+    const user = JSON.parse(cookies.get('user'))
+    const tenant = JSON.parse(cookies.get('user'))
+    return { user, tenant }
 }
-
-interface TenantType {
-    id: string
-    name: string
-}
-
-interface AuthState {
-    user: UserType | null
-    tenant: TenantType | null
-    setAuth: (user: UserType, tenant: TenantType) => void
-    getAuth: () => { user: UserType | null; tenant: TenantType | null }
-}
-
-export const useAuthStore = create<AuthState>((set, get) => ({
-    user: null,
-    tenant: null,
-
-    setAuth: (user, tenant) => set(() => ({ user, tenant })),
-
-    getAuth: () => {
-        const { user, tenant } = get()
-        return { user, tenant }
-    }
-}))
