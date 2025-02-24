0886b566b5a077290a8153a4afec68f0aebb6820
Maxime Fondu <maximefondu@icloud.com>
2025-02-24 16:55:12 +0100

    type useAuth

--- src/utils/auth-store/index.tsx
+++ src/utils/auth-store/index.tsx
@@ -1,6 +1,19 @@
 import cookies from 'js-cookie'
+
+interface User {
+    id: number
+    firstname: string
+    lastname: string
+    email: string
+}
+
+interface Tenant {
+    id: string
+    name: string
+}
+
 export const useAuth = () => {
-    const user = JSON.parse(cookies.get('user'))
-    const tenant = JSON.parse(cookies.get('user'))
+    const user = JSON.parse(cookies.get('user') ?? '') as User
+    const tenant = JSON.parse(cookies.get('tenant') ?? '') as Tenant
     return { user, tenant }
 }
