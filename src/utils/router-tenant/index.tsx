9ee91c4ff51a5c755934f0a6276f912e2524ffea
Maxime Fondu <maximefondu@icloud.com>
2025-02-25 13:31:00 +0100

    fix route

--- src/utils/router-tenant/index.tsx
+++ src/utils/router-tenant/index.tsx
@@ -1,13 +1,32 @@
+'use client'
+
 import { useAuth } from '../auth-context'
 import { useRouter } from 'next/navigation'
 
 export const getTenantUrl = (href: string): string => {
     const { tenant } = useAuth()
+
+    if (!tenant) {
+        return href
+    }
+
     return `/${tenant.id}${href}`
 }
 
 export const useRouterWithTenant = () => {
     const { tenant } = useAuth()
+
+    if (!tenant) {
+        return {
+            back: () => router.back(),
+            forward: () => router.forward(),
+            prefetch: (href: string) => router.prefetch(`${href}`),
+            push: (href: string) => router.push(`${href}`),
+            replace: (href: string) => router.replace(`${href}`),
+            refresh: (href: string) => router.refresh()
+        }
+    }
+
     const router = useRouter()
 
     return {
